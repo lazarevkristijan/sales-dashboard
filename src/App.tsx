67b4db1611164da19e610385bf0acf341f277c86
lazarevkristijan <lazarevkristijan@gmail.com>
2023-08-26 20:06:31 +0200

    experimenting top bar

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import {
 function App() {
   return (
     <div className="bg-[#96f2ff]">
-      <div className="mx-[100px]">
+      <div className="pt-[60px] mx-auto 2xl:w-[1336px] xl:w-[1080px] lg:w-[824px]">
         <Top />
         <Sales />
         <Orders />
