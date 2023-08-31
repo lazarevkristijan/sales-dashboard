7c40ab3d53605dc2eb248396dff5a223883b9af7
lazarevkristijan <lazarevkristijan@gmail.com>
2023-08-31 18:57:57 +0200

    users basic design

--- sections/Users.tsx
+++ sections/Users.tsx
@@ -1,7 +1,24 @@
+import { RectBtn } from "../components"
+
 const Users = () => {
   return (
     <div className="light-blue1 py-[30px] rou mb-[50px] w-[500px]">
-      Users <p></p>
+      <div className="mx-[20px]">
+        <div className="bg-[#fff] text-center py-[20px] rou mb-[20px]">
+          <span className="block mb-[15px]">Registered users</span>
+          <RectBtn
+            text="2173"
+            color="#CAF0F8"
+          />
+        </div>
+        <div className="bg-[#fff] text-center py-[20px] rou">
+          <span className="block mb-[15px]">Daily visits (Avg)</span>
+          <RectBtn
+            text="160"
+            color="#CAF0F8"
+          />
+        </div>
+      </div>
     </div>
   )
 }
