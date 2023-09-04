1a7c2e4d404bc2249e53de83f3d3d0a59b33ae03
lazarevkristijan <lazarevkristijan@gmail.com>
2023-09-04 15:01:06 +0200

    custom periods menu

--- components/CustomPeriodMenu.tsx
+++ components/CustomPeriodMenu.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react"
+import { PanelButton } from "."
+
 const CustomPeriodMenu = () => {
-  return <div>CustomPeriodMenu</div>
+  const [fromDate, setFromDate] = useState("2023-09-05")
+  const [toDate, setToDate] = useState("2024-09-05")
+
+  const handleFromChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFromDate(e.target.value)
+  }
+
+  const handleToChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setToDate(e.target.value)
+  }
+
+  return (
+    <div className="absolute light-blue1 left-0 sm:left-[300px] top-[355px] sm:top-0 rou smText border-black border-[1px]">
+      <div className="m-[10px] w-[300px]">
+        <PanelButton
+          title="From Date"
+          iconWidth="150"
+        >
+          <input
+            type="date"
+            value={fromDate}
+            onChange={handleFromChange}
+          />
+        </PanelButton>
+
+        <PanelButton
+          title="To Date"
+          iconWidth="150"
+        >
+          <input
+            type="date"
+            value={toDate}
+            onChange={handleToChange}
+          />
+        </PanelButton>
+
+        <div className="p-[20px] light-blue3 flex justify-center items-center rou mb-[10px] hover:cursor-pointer">
+          <p className="">Set Custom Period</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CustomPeriodMenu
--- components/DisplayPeriodsMenu.tsx
+++ components/DisplayPeriodsMenu.tsx
@@ -1,7 +1,14 @@
-import { PanelButton } from "."
+import { useState } from "react"
+import { CustomPeriodMenu, PanelButton } from "."
 import { displayPeriodsOptions } from "../constants"
 
 const DisplayPeriodsMenu = () => {
+  const [isCustomMenu, setIsCustomMenu] = useState(false)
+
+  const handleCustomMenu = () => {
+    setIsCustomMenu(true)
+  }
+
   return (
     <div className="absolute light-blue1 left-0 sm:left-[300px] top-[355px] sm:top-0 rou smText border-black border-[1px]">
       {displayPeriodsOptions.map((option, index) => (
@@ -9,11 +16,15 @@ const DisplayPeriodsMenu = () => {
           key={index}
           className="m-[10px] w-[200px] bg-[#fff] rou"
         >
-          <PanelButton title={`${option.title}`}>
+          <PanelButton
+            title={`${option.title}`}
+            onClick={handleCustomMenu}
+          >
             <span className="mx-auto">{`${option.label}`}</span>
           </PanelButton>
         </div>
       ))}
+      {isCustomMenu && <CustomPeriodMenu />}
     </div>
   )
 }
